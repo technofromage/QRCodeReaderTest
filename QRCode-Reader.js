ed6ee43441c774b946e2c39d6aaadbf428eea692
schoolfromage <slarbuckle@hotmail.com>
2022-07-14 23:54:02 -0700

    loaded and implemented qr_scanner library
    This hasn't been tested yet, but it loads and runs the code

--- QRCode-Reader.js
+++ QRCode-Reader.js
@@ -1,24 +1,34 @@
 //This code was designed to work with the video-example.html to process images to handle QR codes
-//for future developers: look into "barcodeDetector" API, it is only available on MAC and Andriod as of writing
-// it may work faster for being offline
-// last edited on 7/13/22
+// last edited on 7/14/22
 
+import QrScanner from './qr-scanner.min.js'
+
+	//this function only exists for me to tell if the functions are loading
 function test(){
 	console.log("hello!")
 }
 
+	//this function interfaces all the HTML stuff with the tools in this library
+function processImage(){
+	const picture = document.getElementById("i");
+	let scanResult = scan(picture)
+	document.getElementById("result").textContent = scanResult
+}
+window.processImage = processImage
 
-function scan(){
-	let xhr = new XMLHttpRequest();
-	xhr.open('POST', "https://api.qrserver.com/v1/create-qr-code/?data=HelloWorld",async=false )
-	xhr.setRequestHeader
-	xhr.getResponseHeader("Access-Control-Allow-Origin","<origin>")
-	// xhr.getResponseHeader('Content-type', 'application/json')
-
-	// file = new File()
-	// fileURL = URL.createObjectURL(file);
-	// console.log(fileURL)
-	// let params = ``
-	xhr.send()//Figure out how to do this correctly
-	console.log("data sent")
+	//takes a blob image/file and returns a string output for the qr code data, or an empty string if no data is read
+function scan(image){
+	QrScanner.scanImage(image, {returnDetailedScanResult: true})
+		.then(
+			result=>{//TODO: process the result to see errors that don't throw (like QR code not found)
+				console.log(result)
+				return result.data
+			}
+			)
+		.catch(
+			error => {
+				console.log(error || 'QR code error')
+				return ""
+			}
+			);
 }
